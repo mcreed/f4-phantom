56ada96f83737a2a3d2049551ebeba8b7a88be7a
Matt Reed <mcreed@Lindseys-MacBook-Pro-3.local>
2015-04-26 22:53:19 -0500

    Get remote content

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var phantom = require('phantom');
 var express = require('express');
 var app = express();
 
@@ -5,7 +6,25 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+	
+	// Set page to phantom
+	var page_url = "http://mcreed.com";
+	if(request.query.linky){
+		page_url = request.query.linky;
+	}
+
+	phantom.create(function (ph) {
+		ph.createPage(function (page) {
+			page.open(page_url, function (status) {
+
+				console.log("opened url? ", status);
+				page.getContent(function (content) {
+					response.send(content);
+				});
+
+		    });
+		});
+	});
 });
 
 app.listen(app.get('port'), function() {
